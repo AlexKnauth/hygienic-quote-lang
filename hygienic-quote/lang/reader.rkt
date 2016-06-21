e6148a0b704e397311d89512ea4856f061bd58ac
AlexKnauth <alexander@knauth.org>
2016-06-21 08:40:16 -0400

    reader-module-paths -> lang-reader-module-paths

--- hygienic-quote/lang/reader.rkt
+++ hygienic-quote/lang/reader.rkt
@@ -5,14 +5,14 @@
                      [hygienic-quote-get-info get-info]))
 
 (require (only-in syntax/module-reader make-meta-reader)
-         (only-in lang-extension/meta-reader-util reader-module-paths)
+         (only-in lang-extension/meta-reader-util lang-reader-module-paths)
          (only-in "../reader.rkt" wrap-reader))
 
 (define-values (hygienic-quote-read hygienic-quote-read-syntax hygienic-quote-get-info)
   (make-meta-reader
    'hygienic-quote
    "language path"
-   reader-module-paths
+   lang-reader-module-paths
    wrap-reader ; for read
    wrap-reader ; for read-syntax
    (λ (get-info) get-info)))
